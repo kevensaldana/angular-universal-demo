e95a48350c5a86c929074d4292e48c5a372e2a0a
keven <keven.sa17@gmail.com>
2020-04-11 13:15:50 -0500

    feat: sitemap and robots.txt

---
+++ src/app/features/public/views/home/infrastructure/containers/character-list/character-list.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-character-list',
+  styleUrls: ['./character-list.component.scss'],
+  templateUrl: './character-list.component.html'
+})
+export class CharacterListComponent {
+
+}
