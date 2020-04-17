91344b009a3584504d5b4108f6f993dc95bec39c
keven <keven.sa17@gmail.com>
2020-04-16 23:12:50 -0500

    variables angular image

--- execute-ssr.sh
+++ execute-ssr.sh
@@ -3,6 +3,6 @@ ng build --prod && ng run demo-angular-universal:server:production
 
 docker build --network=host -f SSR.Dockerfile -t angular-universal .
 
-docker run --rm -p 4000:4000  -e AM_TS='AM_TS' \
-                          -e AM_KEY='AM_KEY' \
-                          -e AM_HASH='AM_HASH' angular-universal
+docker run --rm -p 4000:4000  -e AM_TS='keven' \
+                          -e AM_KEY='f9fa874a9bee4b1082b61f1a20eb5424' \
+                          -e AM_HASH='7a449c39af2b9552d4535b5f7e855174' angular-universal
