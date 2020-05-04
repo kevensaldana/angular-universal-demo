87dd9e2775c2676dbb0319b43f1e552ab4fdd055
keven <keven.sa17@gmail.com>
2020-05-03 22:32:49 -0500

    fix: safari object notification

--- src/app/shared/service-worker/domain/request-permission-notification.ts
+++ src/app/shared/service-worker/domain/request-permission-notification.ts
@@ -6,12 +6,12 @@ import {BehaviorSubject} from 'rxjs';
   providedIn: 'root',
 })
 export class RequestPermissionNotification {
-  public hasPermission$ = new BehaviorSubject<boolean>(isPlatformBrowser(this.platformId) && Notification.permission === 'granted');
+  public hasPermission$ = new BehaviorSubject<boolean>(isPlatformBrowser(this.platformId) && ('Notification' in window) && Notification.permission === 'granted');
   constructor(@Inject(PLATFORM_ID) private readonly platformId) {
 
   }
   request() {
-    if (isPlatformBrowser(this.platformId)) {
+    if (isPlatformBrowser(this.platformId) && ('Notification' in window)) {
       Notification.requestPermission((permission) => {
         this.hasPermission$.next(permission === 'granted');
       }).then();
